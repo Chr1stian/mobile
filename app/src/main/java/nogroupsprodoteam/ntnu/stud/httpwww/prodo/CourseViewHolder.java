123053f34008df20d858825501f8eda3439b72a5
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-03-30 13:48:49 +0200

    first clickable recycleview takes you to next activity

--- app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/CourseViewHolder.java
+++ app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/CourseViewHolder.java
@@ -4,16 +4,28 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import static nogroupsprodoteam.ntnu.stud.httpwww.prodo.CourseAdapter.clickListener;
+
 /**
  * Created by Christian on 29.03.2017.
  */
 
-class CourseViewHolder extends RecyclerView.ViewHolder {
+class CourseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     public final TextView courseNameTextView;
     public final TextView courseCodeTextView;
+
+
     public CourseViewHolder(View itemView) {
         super(itemView);
         this.courseNameTextView = (TextView) itemView.findViewById(R.id.courseName);
         this.courseCodeTextView = (TextView) itemView.findViewById(R.id.courseCode);
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(clickListener!=null){
+            clickListener.itemClicked(v, getAdapterPosition());
+        }
     }
 }
