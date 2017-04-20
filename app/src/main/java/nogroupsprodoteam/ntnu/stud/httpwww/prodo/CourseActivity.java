31654f576c1596af7e89130c89aca57fb83eb055
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-04-20 14:00:53 +0200

    users questions can now be accsessed from every activity

--- app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/CourseActivity.java
+++ app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/CourseActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,6 +27,8 @@ public class CourseActivity extends AppCompatActivity implements LectureAdapter.
     private ArrayList<Question> questionArrayList;
     private int selectedCourseID;
     private List<Lecture> lecturesFromSelectedCourse = new ArrayList<>();
+    private int userID;
+    private String nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,8 @@ public class CourseActivity extends AppCompatActivity implements LectureAdapter.
         topicArrayList = (ArrayList<Topic>) extras.getSerializable("TopicList");
         questionArrayList = (ArrayList<Question>) extras.getSerializable("QuestionList");
         selectedCourseID = extras.getInt("SelectedCourseID");
+        userID = extras.getInt("UserID");
+        nickname = extras.getString("NickName");
 
         TextView lbl_course = (TextView) findViewById(R.id.lbl_course);
         lbl_course.setText(courseArrayList.get(selectedCourseID - 1).getCourseCode() + " - " + courseArrayList.get(selectedCourseID - 1).getCourseName());
@@ -71,6 +77,8 @@ public class CourseActivity extends AppCompatActivity implements LectureAdapter.
         extras.putSerializable("SelectedLecture", selectedLecture);
         extras.putInt("SelectedCourseID", selectedCourseID);
         extras.putInt("SelectedLectureID", selectedLectureID);
+        extras.putInt("UserID", userID);
+        extras.putString("NickName", nickname);
         intent.putExtras(extras);
         startActivity(intent);
     }
@@ -79,4 +87,36 @@ public class CourseActivity extends AppCompatActivity implements LectureAdapter.
     public void itemClicked(View view, int position) {
         sendMessage(position);
     }
+
+    //Creates icon in actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Handles button/icon click activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.mybutton) {
+            openUsersQuestions();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //opens activity displaying the users asked questions
+    public void openUsersQuestions(){
+        Intent intent = new Intent(this, QuestionActivity.class);
+        Bundle extras = new Bundle();
+        extras.putInt("UserID", userID);
+        extras.putString("NickName", nickname);
+        extras.putSerializable("CourseList", courseArrayList);
+        extras.putSerializable("LectureList", lectureArrayList);
+        extras.putSerializable("TopicList", topicArrayList);
+        intent.putExtras(extras);
+        startActivity(intent);
+    }
 }
+
--- app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/QuestionsViewHolder.java
+++ app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/QuestionsViewHolder.java
@@ -2,22 +2,21 @@ package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 /**
- * Created by thea_ on 30.03.2017.
+ * Created by Christian on 20.04.2017.
  */
 
-class QuestionViewHolder extends RecyclerView.ViewHolder {
+class QuestionsViewHolder extends RecyclerView.ViewHolder{
     public final TextView questionTextView;
     public final TextView answerTextView;
-    public final Button endorseButton;
+    public final TextView questionsOriginTextView;
 
-    public QuestionViewHolder(View itemView) {
+    public QuestionsViewHolder(View itemView) {
         super(itemView);
         this.questionTextView = (TextView) itemView.findViewById(R.id.Question);
         this.answerTextView = (TextView) itemView.findViewById(R.id.Answer);
-        this.endorseButton = (Button) itemView.findViewById(R.id.btn_qr);
+        this.questionsOriginTextView = (TextView) itemView.findViewById(R.id.QuestionOrigin);
     }
 }
